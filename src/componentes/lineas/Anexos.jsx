0c940110f58f9e3fa07444c8f6a6156179fb57bc
Fredy Mateo <fredymateomellado@gmail.com>
2025-03-06 17:55:43 -0500

    Pendiente

--- src/componentes/lineas/Anexos.jsx
+++ src/componentes/lineas/Anexos.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import { BsFileEarmarkPdf } from 'react-icons/bs';
 
 
-import detallesPDF from '../../assets/imagenes/detalles003.pdf';
+import detallesPDF from '../../assets/imagenes/detalles4.pdf';
 
 function Anexos() {
   const handleOpenPDF = () => {
