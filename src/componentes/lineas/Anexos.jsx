be03f6f818882394cf05649eb1f4b88db4257b11
Fredy Mateo <fredymateomellado@gmail.com>
2025-03-07 16:20:09 -0500

    Actualizacion de plaza de canchacucho

--- src/componentes/lineas/Anexos.jsx
+++ src/componentes/lineas/Anexos.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import { BsFileEarmarkPdf } from 'react-icons/bs';
 
 
-import detallesPDF from '../../assets/imagenes/detalles5.pdf';
+import detallesPDF from '../../assets/imagenes/detalles6.pdf';
 
 function Anexos() {
   const handleOpenPDF = () => {
