7c605aacc4bb15bdb796584269dd603dfb4500ab
Fredy Mateo <fredymateomellado@gmail.com>
2025-03-09 20:45:55 -0500

    Aumentando limite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
+  plugins: [react()],
+  server: {
     port: 3000, // Cambia este número al puerto que desees
-  }
+  },
+  build: {
+    chunkSizeWarningLimit: 2000, // Aumenta el límite de advertencia (ajusta según sea necesario)
+  },
 });
+
+
+
